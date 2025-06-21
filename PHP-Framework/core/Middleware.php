396538e928c3e67699b867533c76734e65ec61a5
ahmkzaman <kzaman.ahm@gmail.com>
2025-06-21 10:37:07 +0600

    Middleware added

---
+++ PHP-Framework/core/Middleware.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Core;
+
+/**
+ * Base Controller class for the application.
+ * This class provides methods to handle requests and responses.
+ */
+
+abstract class Middleware
+{
+    /**
+     * Handle the request before it reaches the controller.
+     *
+     * @param array $request The request data.
+     * @return bool True if the request should proceed, false otherwise.
+     */
+    abstract public function handle(array $request): void;
+}
